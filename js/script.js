b64ff8701982c5838cb07801ac5ee4cfec629a9c
IgorOdaryuk <odaryukigor@gmail.com>
2024-01-28 23:23:54 +0200

    added a footer

--- js/script.js
+++ js/script.js
@@ -56,3 +56,17 @@ const reviewsSlider = new Swiper('.slider-reviews', {
 		prevEl: '.slider-reviews-prev',
 	},
 });
+
+const galleryItems = document.querySelectorAll(".gallery__item");
+
+if (galleryItems.length > 0) {
+	galleryItems.forEach(item => {
+		new Swiper(item, {
+			slidesPerView: 1, 
+			autoplay: {
+				delay: 5000,
+			},
+			effect: 'fade',
+		})
+	})
+}
